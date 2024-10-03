a845aa5ad232a1a0a10ec8086aaae4ebd8232c3d
KiPhuong <21520406@gm.uit.edu.vn>
2024-10-03 16:57:04 +0700

    fix bug path of branch and bound

--- TSP-Branch-and-Bound.cpp
+++ TSP-Branch-and-Bound.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int n, city[100][100], X[100]; 
+int n, city[100][100], X[100], path[100]; 
 int visited[100];
 int cost = INT_MAX, cmin = INT_MAX;
 int d;
@@ -14,6 +15,9 @@ void Input() {
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> city[i][j];
+			if (city[i][j] > 0) {
+				cmin = min(cmin, city[i][j]);
+			}
 		}
 	}
 }
@@ -25,7 +29,13 @@ void Try(int i) {
 			X[i] = j; //Tim duoc thanh pho thu i
 			d += city[X[i - 1]][X[i]]; //cap nhat khoang cach 
 			if (i == n) { //di het thanh pho
-				cost = min(cost, d + city[X[n]][X[1]]); //so sanh voi cost -> be hon thi cap nhat lai
+				//so sanh voi cost -> be hon thi cap nhat lai
+				if (cost > (d + city[X[n]][X[1]])){
+					cost = min(cost, d + city[X[n]][X[1]]);
+					for (int i = 1; i <= n; i++){
+							path[i] = X[i]; //cap nhat path
+					}
+				}				
 			}
 			//danh gia can
 			else if (d + (n - i + 1) * cmin < cost) {
@@ -42,12 +52,13 @@ int main() {
 	cout<<"Using Branch and Bound"<<endl;
 	Input();
 	X[1] = 1; //bat dau tu thanh pho thu 1
+	path[1] = 1;
 	visited[1] = 1; //danh dau thanh pho thu 1 da di qua
 	Try(2); //bat dau tim thanh pho ke tiep
 	cout << "Shortest path: ";
-	for (int i = 1; i <= n + 1; i++) {
-		if (i <= n) cout << X[i] << " ";
-		else cout << "1 \n";
+	for (int i = 1; i<= n; i++){
+		cout << path[i] <<" ";
 	}
+	cout<<"1 \n";
 	cout <<"Min cost: " << cost << endl;
 }
